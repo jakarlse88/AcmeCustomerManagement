c9f1e42629543d30b2b1fdd1bb581c36162339fd
Jon Karlsen <karlsen.jonarild@gmail.com>
2017-12-06 12:54:32 +0100

    Initial commit

---
+++ ACM.Biz/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACM.BL
+{
+	/// <summary>
+	/// Manages a single customer.
+	/// </summary>
+    public class Customer
+    {
+		public int CustomerId { get; set; }
+		public string EmailAddress { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
